1f7cab14a01fa13641596acf18b1aacd7010e3b4
gugaluck <gustavoluckmann@live.com>
2021-03-15 17:42:31 -0700

    Update verifica_login.php

--- verifica_login.php
+++ verifica_login.php
@@ -1,6 +1,17 @@
 <?php
 session_start();
 
+
+if($row == 1) {
+	$_SESSION['nome'] = $nome;
+	header('Location: menu.php');
+	exit();
+
+} else {
+	$_SESSION['nao_autenticado'] = true;
+	header('Location: index.php');
+	exit();
+}/*
 if(!$_SESSION['nome']) {
     header('Location: menu.php');
     exit();
