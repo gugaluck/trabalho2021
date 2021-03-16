92ff7d60d40e4a1e72bc1a9e8a633dd8254e442b
gugaluck <gustavoluckmann@live.com>
2021-03-16 13:39:03 -0700

    Update login.php

--- login.php
+++ login.php
@@ -3,24 +3,23 @@ session_start();
 include('conexao.php');
 
 if(empty($_POST['nome']) || empty($_POST['senha'])) {
-    header('location: index.php');
-    exit();
+	header('Location: index.php');
+	exit();
 }
 
 $nome = mysqli_real_escape_string($conexao, $_POST['nome']);
 $senha = mysqli_real_escape_string($conexao, $_POST['senha']);
 
-$query = "select * from usuario where nome = ('{$nome}') and senha = md5('{$senha}')";
+$query = ("SELECT * FROM usuario WHERE nome='$nome' AND senha='$senha'") or die("Nome ou senha incorretos");
 $result = mysqli_query($conexao, $query);
 $row = mysqli_num_rows($result);
 
-
 if($row == 1) {
 	$_SESSION['nome'] = $nome;
-	header('Location: painel.php');
+	header('Location: menu.php');
 	exit();
 } else {
+	$_SESSION['nao_autenticado'] = true;
 	header('Location: index.php');
 	exit();
 }
-?>
