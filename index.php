da933b72a6871af7f24dfd25bd9c7bae02383374
gugaluck <gustavoluckmann@live.com>
2021-03-24 20:29:06 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,58 +1,32 @@
+<h3> LOGIN </h3>
 <?php
-session_start();
-require_once __DIR__."/libs/Html.class.php";
-require_once __DIR__."/libs/Body.class.php";
-require_once __DIR__."/libs/Div.class.php";
-require_once __DIR__."/libs/Meta.class.php";
-require_once __DIR__."/libs/Head.class.php";
-require_once __DIR__."/libs/Title.class.php";
-/* 
-$metaCharset = new Meta("UTF-8");
-$metaHttEquiv = new Meta(null, null, "X-UA-Compatible", "IE=edge");
-$metaName = new Meta(null, "viewport", null, "width=device-width, initial-scale=1.0");
+    include('config.php');
+    if(isset($_POST['acao'])){
+        $nome = $_POST['nome'];
+        $senha = $_POST['senha'];
 
-$title = new Title("Login");
+        $sql = $pdo->prepare("select * from usuario where (nome = '{$nome}') and (senha = '{$senha}')");
+        $sql->execute([$nome]);
 
-$head = new Head();
-$head->addElement($metaCharset);
-$head->addElement($metaHttEquiv);
-$head->addElement($metaName);
-$head->addElement($title);
-
-$body = new Body("body");
-echo $title;
-$container = new Div("container");
-
-$html = new Html("pt-br", $head, $body);
-
-echo $html;
-*/
-if(isset($_SESSION['nao_autenticado'])):
+        if($sql->rowCount() == 1){
+            $info = $sql->fetch();
+            echo $info['senha'];
+            if(password_verify($senha, password_hash($info['senha'], PASSWORD_DEFAULT))) {
+                $_SESSION['login'] = true;
+                $_SESSION['id'] = $info['id'];
+                $_SESSION['nome'] = $info['nome'];
+                header("Location: menu.php");
+                die();
+            }else{
+                echo 'Nome ou senha incorretos!';
+            }
+        }else{
+            echo 'Nome não encontrado!';
+        }
+    }
 ?>
-<div>
-    <p>Nome ou senha inválidos</p>
-</div>
-<?php
-    endif;
-    unset($_SESSION['nao_autenticado']);
-?>
-<h3>LOGIN</h3>
-<div class="box">
-    <form action="login.php" method="POST">
-        <div class="campo">
-            <div class="control">
-                <input name="nome" name="text" placeholder="Seu nome" autofocus="">
-            </div>
-        </div>
-
-        <div class="campo">
-            <div class="control">
-                <input name="senha" type="password" placeholder="Sua senha">
-            </div>
-        </div>
-        <button type="submit">Entrar</button>
-    </form>
-</div>
-</body>
- 
-</html>
+<form method="post">
+    <input type="text" name="nome" placeholder="Nome">
+    <input type="password" name="senha" placeholder="Senha">
+    <input type="submit" value="Entrar" name="acao">
+</form>
