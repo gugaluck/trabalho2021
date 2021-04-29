a6def03e08598fd53f27d8261c52e61eff3a806e
gugaluck <gustavoluckmann@live.com>
2021-04-29 14:33:02 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,29 +1,39 @@
 <h3> LOGIN </h3>
 <?php
-    include('config.php');
-    if(isset($_POST['acao'])){
-        $nome = $_POST['nome'];
-        $senha = $_POST['senha'];
+include('config.php');
+if(isset($_POST['acao'])){
+    $nome = $_POST['nome'];
+    $senha = $_POST['senha'];
 
-        $sql = $pdo->prepare("select * from usuario where (nome = '{$nome}') and (senha = '{$senha}')");
-        $sql->execute([$nome]);
+    $sql = "select * from usuario where (nome = '{$nome}') and (senha = '{$senha}')";
+    $sql = $pdo->prepare($sql);
+    $sql->execute();
 
-        if($sql->rowCount() == 1){
-            $info = $sql->fetch();
-            echo $info['senha'];
-            if(password_verify($senha, password_hash($info['senha'], PASSWORD_DEFAULT))) {
-                $_SESSION['login'] = true;
-                $_SESSION['id'] = $info['id'];
-                $_SESSION['nome'] = $info['nome'];
-                header("Location: menu.php");
-                die();
-            }else{
-                echo 'Nome ou senha incorretos!';
-            }
-        }else{
-            echo 'Nome não encontrado!';
-        }
+    try {
+        $conexao = new PDO("mysql:host=localhost; dbname=crudsimples", "root", "123456");
+        $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $conexao->exec("set names utf8");
+    } catch (PDOException $erro) {
+        echo "Erro na conexão:" . $erro->getMessage();
     }
+    
+    if($sql->rowCount() == 1){
+        $info = $sql->fetch();
+        echo $info['senha'];
+        if(password_verify($senha, password_hash($info['senha'], PASSWORD_DEFAULT))) {
+            $_SESSION['login'] = true;
+            $_SESSION['id'] = $info['id'];
+            $_SESSION['nome'] = $info['nome'];
+            header("Location: menu.php");
+            die();
+        }else{
+            echo 'Nome ou senha incorretos!';
+        }
+    }else{
+        echo 'Nome não encontrado!';
+        
+    }
+}
 ?>
 <form method="post">
     <input type="text" name="nome" placeholder="Nome">
